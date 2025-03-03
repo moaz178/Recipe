10ebdf4effccdb20db25afb45a42931bfceee2ca
moazahmad <moaz.ahmad@royalcyber.com>
2025-03-03 23:14:48 +0500

    changes added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+
+  base: '/Recipe/',
   plugins: [react()],
+
+  // Output the build to the docs folder
+  build: {
+    outDir: 'docs',
+  },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
